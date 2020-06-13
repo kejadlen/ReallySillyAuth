ef187622f84d87771001f1f191f47dd9f650c41f
Alpha Chen <alpha@kejadlen.dev>
2020-06-12 18:32:22 -0700

    Initial Commit

---
+++ ReallySillyAuth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReallySillyAuth
+//
+//  Created by Alpha Chen on 6/12/20.
+//  Copyright © 2020 Alpha Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
